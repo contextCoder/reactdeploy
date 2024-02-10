3bb6c9ab72cbdda0a6a1ae907e725c2a6fc29a18
Tukaram Todkari <t.baliram.todkari@accenture.com>
2024-02-10 12:03:26 +0530

    counter setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const counterStatus = ( val) => () => {
+    console.log("======val-------",val);
+    if (val) {
+      if (counter >= 10) {
+        alert("counter should be below '10'")
+      } else {
+        setCounter(counter + 1)
+      }
+    } else {
+      if (counter <= 0) {
+        alert("counter should be above '0'")
+      } else {
+        setCounter(counter - 1 )
+      }
+    }
+
+  }
+  const resetCounter = () => {
+    setCounter(0);
+  }
   return (
     <div className="App">
-      <h4>react app for deploy created by tuka</h4>
+      <h1>react app for deploy created by tuka</h1>
+      <h2>{ counter }</h2>
+      <button onClick={counterStatus(true)}>Increment</button>
+      <button onClick={counterStatus(false)}>Decrement</button>
+      <button onClick={() => resetCounter()}>Reset</button>
     </div>
   );
 }
